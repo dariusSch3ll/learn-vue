b598609025eb59deeb5bf7eb5c92518d9c6b977c
dariusSch3ll <darius.scheller@gmail.com>
2024-07-16 21:38:27 +0200

    exploring event modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
@@ -15,6 +16,12 @@ const app = Vue.createApp({
     setName(event, lastName) {
       this.name = event.target.value + ' ' + lastName
     },
+    submitForm() {
+      alert('submitted')
+    },
+    confirmInput() {
+      this.confirmedName = this.name
+    }
   },
 });
 
